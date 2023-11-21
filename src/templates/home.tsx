efd5b206cb16c20dfed7935704657f9b3ace8e7e
rkeerthient <rkeerthi@yext.com>
2023-11-21 15:14:09 +0530

    updated

--- src/templates/home.tsx
+++ src/templates/home.tsx
@@ -27,7 +27,7 @@ export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
   document,
 }): HeadConfig => {
   return {
-    title: "VUSE | Home",
+    title: "Well | Home",
     charset: "UTF-8",
     viewport: "width=device-width, initial-scale=1",
     tags: [
